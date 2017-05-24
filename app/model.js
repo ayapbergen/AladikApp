678822b2cf6afb93e6d3b691ddd0acb63c69c38a
Adil <aladik95@gmail.com>
2017-05-24 17:04:38 +0600

    First commit

---
+++ app/model.js
@@ -0,0 +1,21 @@
+// Dependencies
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Aladiksdb');
+
+var Schema = mongoose.Schema;
+
+// create a schema
+var postSchema = new Schema({
+    title: String,
+    description: String,
+    image: String,
+    image_id: String,
+    created_at: Date
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Post = mongoose.model('Post', postSchema);
+
+// make this available to our users in our Node applications
+module.exports = Post;
